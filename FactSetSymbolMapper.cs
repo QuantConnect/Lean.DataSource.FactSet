3ac43c9841f51c97d879187051b50e0111191b63
Jhonathan Abreu <jdabreu25@gmail.com>
2024-03-20 17:32:54 -0400

    Add symbol mapper implementation

--- FactSetSymbolMapper.cs
+++ FactSetSymbolMapper.cs
@@ -15,28 +15,35 @@
 */
 
 using System;
+using System.Collections.Generic;
 using QuantConnect.Brokerages;
 
 namespace QuantConnect.Lean.DataSource.FactSet
 {
     /// <summary>
+    /// Provides basic mapping between Lean symbols and FactSet symbols.
+    /// Reference: https://assets.ctfassets.net/lmz2w5z92b9u/2yNjkkO0Q1ZMsBpGQ6KfHn/62008ef87b8c154e99ecd4186e86851a/FactSetTickHistory_DataModel_V1.0E.pdf Section 3.3 "Options Identifiers"
     /// </summary>
     public class FactSetSymbolMapper : ISymbolMapper
     {
+        private readonly Dictionary<string, Symbol> _leanSymbolsCache = new();
+        private readonly Dictionary<Symbol, string> _brokerageSymbolsCache = new();
+        private readonly object _lock = new();
+
         /// <summary>
-        /// Get the FactSet symbol for a given Lean symbol
+        /// Get the FactSet symbol in their OCC21 format for a given Lean symbol
         /// </summary>
         /// <param name="symbol">The Lean symbol</param>
         /// <returns>The FactSet symbol</returns>
         public string GetBrokerageSymbol(Symbol symbol)
         {
-            throw new NotImplementedException();
+            return GetFactSetOCC21Symbol(symbol);
         }
 
         /// <summary>
-        /// Get the Lean symbol for a given FactSet symbol
+        /// Get the Lean symbol for a given FactSet symbol (in their OCC21 format)
         /// </summary>
-        /// <param name="brokerageSymbol">The FactSet symbol</param>
+        /// <param name="brokerageSymbol">The FactSet OCC21 symbol</param>
         /// <param name="securityType">The security type</param>
         /// <param name="market">The market</param>
         /// <param name="expirationDate">The expiration date</param>
@@ -46,7 +53,97 @@ namespace QuantConnect.Lean.DataSource.FactSet
         public Symbol GetLeanSymbol(string brokerageSymbol, SecurityType securityType, string market, DateTime expirationDate = default,
             decimal strike = 0, OptionRight optionRight = OptionRight.Call)
         {
-            throw new NotImplementedException();
+            return ParseFactSetOCC21Symbol(brokerageSymbol, securityType, market);
+        }
+
+        /// <summary>
+        /// Get the FactSet symbol in their OCC21 format for a given Lean symbol
+        /// </summary>
+        /// <param name="symbol">The Lean symbol</param>
+        /// <returns>The FactSet symbol</returns>
+        public string GetFactSetOCC21Symbol(Symbol symbol)
+        {
+            if (symbol == null || string.IsNullOrWhiteSpace(symbol.Value))
+            {
+                throw new ArgumentException($"Invalid symbol: {(symbol == null ? "null" : symbol.ToString())}");
+            }
+
+            lock (_lock)
+            {
+                if (!_brokerageSymbolsCache.TryGetValue(symbol, out var brokerageSymbol))
+                {
+                    switch (symbol.SecurityType)
+                    {
+                        case SecurityType.Equity:
+                        case SecurityType.Index:
+                            brokerageSymbol = symbol.Value;
+                            break;
+
+                        case SecurityType.Option:
+                        case SecurityType.IndexOption:
+                            brokerageSymbol = $"{symbol.Value.Substring(0, 6).TrimEnd()}#{symbol.Value.Substring(6)}";
+                            break;
+
+                        default:
+                            throw new ArgumentException($"Unsupported security type: {symbol.SecurityType}");
+                    }
+
+                    _brokerageSymbolsCache[symbol] = brokerageSymbol;
+                    _leanSymbolsCache[brokerageSymbol] = symbol;
+                }
+
+                return brokerageSymbol;
+            }
+        }
+
+        /// <summary>
+        /// Parse the FactSet symbol in their OCC21 format to a Lean symbol
+        /// </summary>
+        /// <param name="occ21Symbol">The FactSet symbol in OCC21 format</param>
+        /// <param name="securityType">The symbol security type</param>
+        /// <param name="market">The market</param>
+        /// <returns>The corresponding Lean symbol</returns>
+        public Symbol ParseFactSetOCC21Symbol(string occ21Symbol, SecurityType securityType, string market = Market.USA)
+        {
+            if (string.IsNullOrEmpty(occ21Symbol))
+            {
+                throw new ArgumentException($"Invalid OCC21 symbol: {occ21Symbol}");
+            }
+
+            lock (_lock)
+            {
+                if (!_leanSymbolsCache.TryGetValue(occ21Symbol, out var symbol))
+                {
+                    switch (securityType)
+                    {
+                        case SecurityType.Equity:
+                        case SecurityType.Index:
+                            symbol = Symbol.Create(occ21Symbol, securityType, Market.USA);
+                            break;
+
+                        case SecurityType.Option:
+                        case SecurityType.IndexOption:
+                            var parts = occ21Symbol.Split('#');
+                            if (parts.Length != 2)
+                            {
+                                throw new ArgumentException($"Invalid OCC21 symbol: {occ21Symbol}");
+                            }
+
+                            // FactSet symbol might end with "-{Exchange OSI}" (e.g. -US) but it currently only supports US
+                            var osiTicker = parts[0].PadRight(6, ' ') + parts[1].Split("-")[0];
+                            symbol = SymbolRepresentation.ParseOptionTickerOSI(osiTicker, securityType, Market.USA);
+                            break;
+
+                        default:
+                            throw new ArgumentException($"Unsupported security type: {securityType}");
+                    }
+
+                    _leanSymbolsCache[occ21Symbol] = symbol;
+                    _brokerageSymbolsCache[symbol] = occ21Symbol;
+                }
+
+                return symbol;
+            }
         }
     }
 }
---
+++ tests/FactSetSymbolMapperTests.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+using QuantConnect.Util;
+using QuantConnect.Tests;
+using QuantConnect.Lean.DataSource.FactSet;
+using QuantConnect.Securities;
+using System.Collections.Generic;
+
+namespace QuantConnect.DataLibrary.Tests
+{
+    [TestFixture]
+    public class FactSetSymbolMapperTests
+    {
+        private static IEnumerable<Tuple<string, Symbol>> SymbolConvertionTestCases()
+        {
+            // Equity
+            yield return new Tuple<string, Symbol>("SPY", Symbols.SPY);
+
+            // Index
+            yield return new Tuple<string, Symbol>("SPX", Symbols.SPX);
+
+            // Options
+            var msft = Symbol.Create("MSFT", SecurityType.Equity, Market.USA);
+            var msftOption = Symbol.CreateOption(msft, Market.USA, OptionStyle.American, OptionRight.Call, 26m, new DateTime(2009, 11, 21));
+
+            yield return new Tuple<string, Symbol>("MSFT#091121C00026000", msftOption);
+            // Index Options
+
+            var spxOption = Symbol.CreateOption(Symbols.SPX, Market.USA, OptionStyle.American, OptionRight.Put, 5210m, new DateTime(2024, 03, 28));
+            yield return new Tuple<string, Symbol>("SPX#240328P05210000", spxOption);
+        }
+
+        private static IEnumerable<TestCaseData> FactSetToLeanSymbolConversionTestCases()
+        {
+            return SymbolConvertionTestCases().Select(x => new TestCaseData(x.Item1, x.Item2));
+        }
+
+        [TestCaseSource(nameof(FactSetToLeanSymbolConversionTestCases))]
+        public void ConvertsFactSetOCC21SymbolToLeanSymbol(string factSetOcc21Symbol, Symbol expectedLeanSymbol)
+        {
+            var mapper = new FactSetSymbolMapper();
+            var leanSymbol = mapper.GetLeanSymbol(factSetOcc21Symbol, expectedLeanSymbol.SecurityType, Market.USA);
+            Assert.That(leanSymbol, Is.EqualTo(expectedLeanSymbol));
+        }
+
+        private static IEnumerable<TestCaseData> LeanToFactSetSymbolConversionTestCases()
+        {
+            return SymbolConvertionTestCases().Select(x => new TestCaseData(x.Item2, x.Item1));
+        }
+
+        [TestCaseSource(nameof(LeanToFactSetSymbolConversionTestCases))]
+        public void ConvertsLeanSymbolToFactSetOCC21Symbol(Symbol leanSymbol, string expectedFactSetOcc21Symbol)
+        {
+            var mapper = new FactSetSymbolMapper();
+            var factSetOcc21Symbol = mapper.GetBrokerageSymbol(leanSymbol);
+            Assert.That(factSetOcc21Symbol, Is.EqualTo(expectedFactSetOcc21Symbol));
+        }
+    }
+}
